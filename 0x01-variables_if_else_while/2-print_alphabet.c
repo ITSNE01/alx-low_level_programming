22e22db9b3f561ce03c5da0d4dfff65e1cedc50a
ITSNE01 <rabie.haddar1@gmail.com>
2024-06-21 18:51:18 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char A;
+
+	A = 'a';
+	while 
+		(A <= 'z') {
+			putchar(A);
+			A++;
+		}
+	putchar('\n');
+	return (0);
+}
