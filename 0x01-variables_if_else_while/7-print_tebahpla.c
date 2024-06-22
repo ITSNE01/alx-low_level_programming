1b7b9b4c98c689c79c989346f5d9e854ea472a2f
ITSNE01 <rabie.haddar1@gmail.com>
2024-06-21 20:09:48 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * This program prints alphabats in reverse.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Z;
+	for (Z = 'z'; Z >= 'a'; Z--)
+	{
+		putchar(Z);
+	}
+	putchar('\n');
+	return (0);
+}
