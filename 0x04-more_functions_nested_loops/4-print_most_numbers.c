b41c927fc22fbf4f10d721524f538cc70653b51d
ITSNE01 <rabie.haddar1@gmail.com>
2024-06-27 08:16:25 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers besides 2 and 4
+ *
+ * Return: no return
+ */
+
+void print_most_numbers(void)
+{
+	int N;
+
+	for (N = 0; N <= 9; N++) 
+	{
+		if (N != 2 && N != 4)
+		{
+		_putchar(N + '0');
+		}
+	}
+	_putchar('\n');
+}
