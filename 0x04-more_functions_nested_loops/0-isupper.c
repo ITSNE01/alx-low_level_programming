e5e5fd852cc7988fe25bd88a8d7da61f38bbcb5a
ITSNE01 <rabie.haddar1@gmail.com>
2024-06-27 06:23:51 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - Dettect UPPERCASE
+ * @c: input
+ * Return: 1 for true, 0 for false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
