588a10510d03970440ce7e957d72f36836d69ff5
ITSNE01 <rabie.haddar1@gmail.com>
2024-07-15 14:26:54 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+int main (int argc, char *argv[]);
+
+#endif
